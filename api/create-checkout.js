bdbb40d5402d32c360f1f6895894b4c998ea5fae
timberlanddigitaldesign-cpu <timberlanddigitaldesign@gmail.com>
2026-06-26 01:31:26 -0700

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -88,6 +88,9 @@ module.exports = async (req, res) => {
       mode,
       line_items: lineItems,
       customer_email: customerEmail || undefined,
+      subscription_data: hasSubscription ? {
+        billing_cycle_anchor_config: { type: 'now' },
+      } : undefined,
       metadata: {
         business_name:         businessName || '',
         customer_name:         customerName || '',
